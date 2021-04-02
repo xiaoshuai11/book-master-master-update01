16b41c85a25f7c94ffb2af9f7bc125033bd01685
xiaoshuai11 <zhangshuai_alex@163.com>
2021-04-02 11:11:24 +0800

    第一版

---
+++ readme.md
@@ -0,0 +1,230 @@
+# 毕业设计--基于Django的图书推荐系统和论坛
+【联系方式：微信 1257309054】
+
+[demo]( )
+
+拉取项目后，启动项目步骤如下
+
+## 1、修改book/settings.py中的数据库名称
+
+```
+先在本地mysql创建一个名为book_master的数据库
+然后按以下图片所示找到相应位置，修改连接mysql的用户名与密码
+```
+
+![1587389870694](image/1587389870694.png)
+
+## 2、创建虚拟环境
+
+```
+使用pycharm创建虚拟环境
+打开pycharm,依次点击左上角File->settings->Project:book-master->Project Interpreter
+```
+
+![1583056468304](image/1583056468304.png)
+
+## 3、安装依赖库
+
+```
+打开Pycharm左下角的Terminal
+输入命令
+pip install -r requirements.txt
+
+```
+
+## 4、创建表数据迁移
+
+```
+打开pycharm左上角的Tools->Run manage.py Task
+依次输入命令
+makemigrations
+migrate
+
+```
+
+等数据迁移完成后，创建超级管理员用于登录后台管理系统
+
+```
+打开pycharm左上角的Tools->Run manage.py Task
+输入命令
+createsuperuser
+```
+
+## 5、上架图书
+
+方式一：
+
+```
+打开Pycharm左下角的Terminal
+输入命令
+python manage.py runserver
+```
+
+然后打开浏览器
+
+```
+输入地址
+http://127.0.0.1:8000/create_book/
+这个是生成一些常用的书籍
+下面这个是后台手动上架图书
+http://127.0.0.1:8000/admin/
+```
+
+![1583057347009](image/1583057347009.png)
+
+
+
+方式二：
+
+也可以直接运行book.sql中的语句直接新增书籍。
+
+
+
+## 6、进入用户访问界面
+
+打开浏览器
+
+```
+输入地址
+http://127.0.0.1:8000/
+```
+
+至此，整个项目运行完成
+
+
+
+
+
+## feature
+
+1.	登录注册页面
+	.	基于协同过滤的图书的分类，排序，搜索，打分功能
+	.	基于协同过滤的周推荐和月推荐
+
+4. 读书分享会等活动功能，用户报名功能
+5. 发帖留言论坛功能
+
+6. 周推荐用户没有评分时随机推荐
+
+7. 按照收藏数量排序
+
+
+
+### 界面
+
+![1584710625859](image/界面.png)
+
+### 注册和登录
+
+
+
+![注册](./image/register.png)
+
+
+
+### 推荐
+
+
+
+![](./image/mdwxj.png)
+
+
+
+### 论坛
+
+![1584711445272](image/论坛.png)
+
+
+
+### 周推荐
+
+
+
+![周推荐](./image/ztj.png)
+
+
+
+
+
+## 使用redis+celery做分布式
+
+### 1、解压redis-64.2.8.2101.rar到D盘
+
+然后启动redis,直接双击【启动redis.bat】
+
+如果是解压redis到其它文件夹，则使用文本打开【启动redis.bat】，然后修改启动路径
+
+![1583568620010](image/启动redis.png)
+
+
+
+### 2、安装第三方库
+
+```python
+pip install -r requirements.txt
+```
+
+### 3、数据库迁移
+
+```python
+打开pycharm左上角的Tools->Run manage.py Task
+依次输入命令
+makemigrations
+migrate
+```
+
+
+
+### 4、启动项目
+
+```python
+打开Pycharm左下角的Terminal
+输入命令
+python manage.py runserver
+```
+
+
+
+### 5、启动celery分布式
+
+![1583568968675](image/terminal.png)
+
+```python
+打开第二个Terminal
+输入命令(可以把下面命令复制到Terminal，然后回车)
+celery -A book beat -s "celery_app/celerybeat-schedule" --pidfile=
+
+```
+
+```python
+打开第三个Terminal
+输入命令(可以把下面命令复制到Terminal，然后回车)，实现对celery的监督
+celery -A book worker -l debug -P eventlet
+```
+
+
+
+对celery有不懂的可以查看网站[celery](https://blog.csdn.net/lm_is_dc/article/details/82705450)
+
+
+
+https://blog.csdn.net/lm_is_dc/article/details/82705450
+
+
+
+## 后记
+
+【后记】为了让大家能够轻松学编程，我创建了一个公众号【轻松学编程】，里面有让你快速学会编程的文章，当然也有一些干货提高你的编程水平，也有一些编程项目适合做一些课程设计等课题。
+
+也可加我微信【1257309054】，拉你进群，大家一起交流学习。
+如果文章对您有帮助，请我喝杯咖啡吧！
+
+公众号
+
+![公众号](https://img-blog.csdnimg.cn/20200317124808234.jpg)
+
+
+
+![赞赏码](https://img-blog.csdnimg.cn/20200317125156789.png?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L2xtX2lzX2Rj,size_16,color_FFFFFF,t_70)
+
+关注我，我们一起成长~~
---
+++ recommend_books.py
@@ -0,0 +1,142 @@
+# -*-coding:utf-8-*-
+import os
+import django
+import operator
+from user.models import *
+from math import sqrt, pow
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "book.settings"
+django.setup()
+
+
+class UserCf:
+    # 基于用户协同算法来获取推荐列表
+    """
+    利用用户的群体行为来计算用户的相关性。
+    计算用户相关性的时候我们就是通过对比他们对相同物品打分的相关度来计算的
+
+    举例：
+
+    --------+--------+--------+--------+--------+
+            |   X    |    Y   |    Z   |    R   |
+    --------+--------+--------+--------+--------+
+        a   |   5    |    4   |    1   |    5   |
+    --------+--------+--------+--------+--------+
+        b   |   4    |    3   |    1   |    ?   |
+    --------+--------+--------+--------+--------+
+        c   |   2    |    2   |    5   |    1   |
+    --------+--------+--------+--------+--------+
+
+    a用户给X物品打了5分，给Y打了4分，给Z打了1分
+    b用户给X物品打了4分，给Y打了3分，给Z打了1分
+    c用户给X物品打了2分，给Y打了2分，给Z打了5分
+
+    那么很容易看到a用户和b用户非常相似，但是b用户没有看过R物品，
+    那么我们就可以把和b用户很相似的a用户打分很高的R物品推荐给b用户，
+    这就是基于用户的协同过滤。
+    """
+
+    # 获得初始化数据
+    def __init__(self, data):
+        self.data = data
+
+    # 通过用户名获得书籍列表，仅调试使用
+    def getItems(self, username1, username2):
+        return self.data[username1], self.data[username2]
+
+    # 计算两个用户的皮尔逊相关系数
+    def pearson(self, user1, user2):  # 数据格式为：书籍id，浏览次数
+        print("user message", user1)
+        sumXY = 0.0
+        n = 0
+        sumX = 0.0
+        sumY = 0.0
+        sumX2 = 0.0
+        sumY2 = 0.0
+        for movie1, score1 in user1.items():
+            if movie1 in user2.keys():  # 计算公共的书籍浏览次数
+                n += 1
+                sumXY += score1 * user2[movie1]
+                sumX += score1
+                sumY += user2[movie1]
+                sumX2 += pow(score1, 2)
+                sumY2 += pow(user2[movie1], 2)
+        if n == 0:
+            print("p氏距离为0")
+            return 0
+        molecule = sumXY - (sumX * sumY) / n
+        denominator = sqrt((sumX2 - pow(sumX, 2) / n) * (sumY2 - pow(sumY, 2) / n))
+        if denominator == 0:
+            print("共同特征为0")
+            return 0
+        r = molecule / denominator
+        print("p氏距离:", r)
+        return r
+
+    # 计算与当前用户的距离，获得最临近的用户
+    def nearest_user(self, username, n=1):
+        distances = {}
+        # 用户，相似度
+        # 遍历整个数据集
+        for user, rate_set in self.data.items():
+            # 非当前的用户
+            if user != username:
+                distance = self.pearson(self.data[username], self.data[user])
+                # 计算两个用户的相似度
+                distances[user] = distance
+        closest_distance = sorted(
+            distances.items(), key=operator.itemgetter(1), reverse=True
+        )
+        # 最相似的N个用户
+        print("closest user:", closest_distance[:n])
+        return closest_distance[:n]
+
+    # 给用户推荐书籍
+    def recommend(self, username, n=1):
+        recommend = {}
+        nearest_user = self.nearest_user(username, n)
+        for user, score in dict(nearest_user).items():  # 最相近的n个用户
+            for book_id, scores in self.data[user].items():  # 推荐的用户的书籍列表
+                if book_id not in self.data[username].keys():  # 当前username没有看过
+                    rate = Rate.objects.filter(book_id=book_id, user__username=user)
+                    # 如果用户评分低于3分，则表明用户不喜欢此书籍，则不推荐给别的用户
+                    if rate and rate.first().mark < 3:
+                        continue
+                    if book_id not in recommend.keys():  # 添加到推荐列表中
+                        recommend[book_id] = scores
+        # 对推荐的结果按照书籍浏览次数排序
+        return sorted(recommend.items(), key=operator.itemgetter(1), reverse=True)
+
+
+def recommend_by_user_id(user_id):
+    # 通过用户协同算法来进行推荐
+    current_user = User.objects.get(id=user_id)
+    # 如果当前用户没有打分 则按照热度顺序返回
+    if current_user.rate_set.count() == 0:
+        book_list = Book.objects.all().order_by("-sump")[:15]
+        return book_list
+    users = User.objects.all()
+    all_user = {}
+    for user in users:
+        rates = user.rate_set.all()
+        rate = {}
+        # 用户有给图书打分
+        if rates:
+            for i in rates:
+                rate.setdefault(str(i.book.id), i.mark)
+            all_user.setdefault(user.username, rate)
+        else:
+            # 用户没有为书籍打过分，设为0
+            all_user.setdefault(user.username, {})
+
+    print("this is all user:", all_user)
+    user_cf = UserCf(data=all_user)
+    recommend_list = user_cf.recommend(current_user.username, 15)
+    good_list = [each[0] for each in recommend_list]
+    print('this is the good list', good_list)
+    if not good_list:
+        # 如果没有找到相似用户喜欢的书则按照热度顺序返回
+        book_list = Book.objects.all().order_by("-sump")[:15]
+        return book_list
+    book_list = Book.objects.filter(id__in=good_list).order_by("-sump")[:15]
+    return book_list
